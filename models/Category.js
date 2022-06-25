4a23fe4720d0163a18ffc88d8249a485aea23d06
nruffin95 <nruffin95@gmail.com>
2022-06-24 23:33:40 -0500

    Added model mockup for database

--- models/Category.js
+++ models/Category.js
@@ -5,15 +5,25 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
+  // TABLE COLUMN DEFINITIONS GO HERE
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,25 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [
+        {
+          model: Product,
+          // required: true
+        },
+      ],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "Categories cannot be found!" });
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
